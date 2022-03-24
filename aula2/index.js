7ee25b41de571a4177249abcf4b7eb9f5eb32f17
SamuelVBF <samuel.bjj@hotmail.com>
2022-03-24 17:17:17 -0300

    first commit

---
+++ aula2/index.js
@@ -0,0 +1,13 @@
+/* 
+Operadores de comparação 
+
+> maior que
+>= maior ou igual
+< menor que
+<= menor ou igual a
+== igualdade
+=== igualdade estrita (valor e tipo)
+!= diferente (valor)
+!== diferente ertrito (valor e tipo)
+*/
+
